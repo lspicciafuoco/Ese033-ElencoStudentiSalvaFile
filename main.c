e9076bcd3296be5d1347a40672138cd53b3dbb21
Alessio Picciafuoco <alessio.picciafuoco@gmail.com>
2015-04-22 22:09:18 +0200

    commit

--- main.c
+++ main.c
@@ -1,20 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define MAX_STRLEN 30
+#define N 3
 
-// Definisci le costanti
+struct s_studente {
+    char nome[MAX_STRLEN+1];
+    char cognome[MAX_STRLEN+1];
+    unsigned int eta;
+    char classe[MAX_STRLEN+1];
+};
 
-// Definisci il tipo di dato struct s_studente
-
-// Definisci studente come struct s_studente
-
+struct s_studente elenco[N];
 
 int main(int argc, char** argv) {
-    // Dichiara le variabili locali
+
+    int i;
+    FILE *puntaFile;
+     
+    for(i = 0; i < N; i++) {
+        printf("Nome %d : ", i);
+        scanf("%s", elenco[i].nome);
+        printf("Cognome elemento %d : ", i);
+        scanf("%s", elenco[i].cognome);
+        printf("Eta' %d : ", i);
+        scanf("%d", &(elenco[i].eta));
+        printf("Classe %d : ", i);
+        scanf("%s", elenco[i].classe);
+    }
     
-    // Codice per l'inserimento, da parte dell'utente, dei dati in un array
-    
-    // Codice per la scrittura dell'elenco su file
+  
+    puntaFile = fopen("elenco.dat", "wb");
+    if(puntaFile != NULL) {
+        fwrite(&elenco, sizeof(elenco), N, puntaFile);
+        fclose(puntaFile);
+    }
+    else
+        printf("Impossibile aprire il file\n");
     
     return (EXIT_SUCCESS);
 }
--- nbproject/Makefile-impl.mk
+++ nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=Ese033-ElencoStudentiSalvaFile
+PROJECTNAME=033
 
 # Active Configuration
 DEFAULTCONF=Debug
